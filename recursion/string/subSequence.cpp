79fe1af4cd3738e023ee44b8f06f267e108df3c8
du2711 <deepanshuupadhyay603@gmail.com>
2026-07-03 14:27:45 +0530

    sub sequence for strings

---
+++ recursion/string/subSequence.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+
+    void subseq(string &arr,int idx,string sum,vector<string> &result){
+    if(idx==arr.size()){
+        result.push_back(sum);
+        return;
+    }
+    subseq(arr,idx+1,sum+arr[idx],result);
+    subseq(arr,idx+1,sum,result);
+}
+
+int main(){ 
+    string str;
+    cout<<"Enter string: ";
+    getline(cin,str);
+
+    vector<string> res;
+    subseq(str,0,"",res);
+
+    for (string s : res) {
+        cout<<endl<<s;
+    }
+    return 0;
+}
