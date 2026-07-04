22c7b436eb2c6392e263c45e29b1975aa87be5d9
du2711 <deepanshuupadhyay603@gmail.com>
2026-07-04 13:42:28 +0530

    work case for zero in mid

--- recursion/numCombinationMobile.cpp
+++ recursion/numCombinationMobile.cpp
@@ -10,6 +10,7 @@ void comb(string &str,int idx,string result,vector<string> &li,vector<string> &v
     int digit = str[idx] - '0';
     if(digit <= 1){
         comb(str,idx+1,result,li,v);
+        return;
     }
     for(int j=0; j<v[digit].size(); j++){
         comb(str,idx+1,result+v[digit][j],li,v);
