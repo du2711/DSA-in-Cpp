378c8924f143e48cc0df6e96aad671be5dc48ea6
du2711 <deepanshuupadhyay603@gmail.com>
2026-07-17 12:16:27 +0530

    implemented merge sort

--- sorting/sorting Algo/mergeSort.cpp
+++ sorting/sorting Algo/mergeSort.cpp
@@ -2,8 +2,48 @@
 #include<vector>
 using namespace std;
 
-vector<int> mergeSort(vector<int> &arr){
+void merge(vector<int> &arr,int l,int mid,int r){
+    int an =mid-l+1;
+    int bn = r-mid;
 
+    vector<int> a(an);
+    vector<int> b(bn);
+
+    for(int i=0;i<an;i++){
+        a[i]=arr[l+i];
+    }
+    for(int j=0;j<bn;j++){
+        b[j]=arr[mid+1+j];
+    }
+
+    int i=0,j=0,k=l;
+
+    while(i<an && j<bn){
+        if(a[i]<b[j]) arr[k++]=a[i++];
+        else arr[k++]=b[j++];
+    }
+
+    while(i<an) arr[k++]=a[i++];
+    while(j<bn) arr[k++]=b[j++];
+}
+
+void mergesort(vector<int> &arr,int l,int r){
+    // int e=arr.size();
+    // int m=arr.size();
+
+    // if(m%2==0) m=m/2;
+    // else m=(m/2)+1;
+
+    if(l>=r) return;
+
+    int mid=(l+r)/2;
+    mergesort(arr,0,mid);
+    mergesort(arr,mid+1,r);
+    merge(arr,l,mid,r);
+}
+
+vector<int> mergeSort(vector<int> &ar){
+    mergesort(ar,0,ar.size()-1);
 }
 
 int main(){ 
@@ -14,7 +54,7 @@ int main(){
         if(cin.peek()=='\n') break;
     }
 
-    //mergeSort(arr);
+    mergeSort(arr);
 
     for(int e:arr){
         cout<<e<<" ";
