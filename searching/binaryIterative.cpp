83a12f95f82845066dce6ae8edaae372f4ab00cb
du2711 <deepanshuupadhyay603@gmail.com>
2026-07-28 12:08:47 +0530

    made code easier and more profecient

--- searching/binaryIterative.cpp
+++ searching/binaryIterative.cpp
@@ -10,15 +10,9 @@ int binarySearch(const vector<int> &arr,int target){
     while(low<=high){
         int mid=(low+(high-low))/2;
 
-        if(target==arr[mid]){
-            return mid;
-        }
-        else if(target<arr[mid]){
-            high=mid-1;
-        }
-        else if(target>arr[mid]){
-            low=mid+1;
-        }
+        if(target==arr[mid]) return mid;
+        else if(target<arr[mid]) high=mid-1;
+        else low=mid+1;
     }
     return -1;
 }
@@ -28,7 +22,10 @@ int main(){
     int ele;
     while(cin>>ele){
         arr.push_back(ele);
-        if(cin.peek()=='\n') break;
+        if(cin.peek()=='\n') {
+            cin.get();
+            break;
+        }
     }
 
     int target;
