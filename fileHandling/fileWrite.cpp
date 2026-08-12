7abc6b0338997b424c4c5c03e00165ab8615e280
du2711 <deepanshuupadhyay603@gmail.com>
2026-08-12 15:55:33 +0530

    file handling write

--- fileHandling/fileWrite.cpp
+++ fileHandling/fileWrite.cpp
@@ -5,6 +5,21 @@ using namespace std;
 
 int main(){ 
     ofstream outfile;
-    
+    outfile.open("file.txt");
+
+    outfile<<"Next to printed"<<endl;
+    outfile<<"hello";
+
+    outfile.close();
+
+    ifstream infile;
+    infile.open("file.txt");
+
+    string line;
+    while(getline(infile,line)){
+        cout<<line;
+    }
+
+    infile.close();
     return 0;
 }
