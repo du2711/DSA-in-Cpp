93a04196c4a1920c1a89cc6ad3291e6044ec4974
du2711 <deepanshuupadhyay603@gmail.com>
2026-08-19 21:41:06 +0530

    multiple inheritance

--- OOPS/multipleInheritance.cpp
+++ OOPS/multipleInheritance.cpp
@@ -6,21 +6,35 @@ class cricketer{
 public:
     int runs;
     int wickets;
-    int avg;
+    float avg;
 };
 
 class engineer{
+public:
     int experience;
     string domain;
 };
 
-class phodu : public engineer,cricketer{        //multiple inheritance
+class phodu : public engineer , public cricketer{        //multiple inheritance
 public:
     string name;
+
+    void print(){
+        cout<<name<<" "<<runs<<" "<<wickets<<" "<<avg<<" "<<experience<<" "<<domain<<endl;
+    }
 };
 
 int main(){ 
     phodu p;
+    
     p.name="Saourav Netravalkar";
+    p.runs=3000;
+    p.wickets=150;
+    p.avg=43.7;
+    p.experience=5;
+    p.domain="Software Engineer";
+
+    p.print();
+
     return 0;
 }
