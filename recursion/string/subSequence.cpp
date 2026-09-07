0e3f730b5368189c0b5a452b4ee2d691a4dff60c
du2711 <deepanshuupadhyay603@gmail.com>
2026-09-07 21:53:42 +0530

    indentation

--- recursion/string/subSequence.cpp
+++ recursion/string/subSequence.cpp
@@ -3,13 +3,13 @@
 using namespace std;
 
     void subseq(string &arr,int idx,string sum,vector<string> &result){
-    if(idx==arr.size()){
-        result.push_back(sum);
-        return;
+        if(idx==arr.size()){
+            result.push_back(sum);
+            return;
+        }
+        subseq(arr,idx+1,sum+arr[idx],result);
+        subseq(arr,idx+1,sum,result);
     }
-    subseq(arr,idx+1,sum+arr[idx],result);
-    subseq(arr,idx+1,sum,result);
-}
 
 int main(){ 
     string str;
